e50f7443a11e1e43ff5da9cb54de07581341f79c
M-kip <93698784+M-kip@users.noreply.github.com>
2025-09-17 10:52:20 +0000

    Add string transformation functions: toCamelCase, transformString, and to_snake_case

---
+++ few_shot_prompt.js
@@ -0,0 +1,14 @@
+// few_shot_prompt.js
+// This file was created as requested.
+function toCamelCase(str) {
+    return str
+        .toLowerCase()
+        .replace(/[_\-\s]+(.)?/g, (_, chr) => chr ? chr.toUpperCase() : '')
+        .replace(/^[A-Z]/, chr => chr.toLowerCase());
+}
+
+// Example usage:
+// console.log(toCamelCase('first name')); // firstName
+// console.log(toCamelCase('user_id')); // userId
+// console.log(toCamelCase('SCREEN_NAME')); // screenName
+// console.log(toCamelCase('mobile-number')); // mobileNumber
